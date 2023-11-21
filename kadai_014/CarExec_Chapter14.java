003eafa51bfb82a65f89dc755bfa072829338751
coreyhig <143170162+coreyhig@users.noreply.github.com>
2023-11-21 15:07:03 +0900

    Add files via upload

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -1,12 +1,18 @@
 package kadai_014;
-	public class CarExec_Chapter14{
-		public static void main(String[] args) {
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// 車クラスのインスタンスを作成する
+		Car_Chapter14 gear = new Car_Chapter14(1,10);
+		Car_Chapter14 speed = new Car_Chapter14(3,30); 		
 		
-			//車クラスのインスタンス
-			Car_Chapter14 car = new Car_Chapter14();
+		// 車クラスのgearChangeメソッドを実行する
+		gear.gearChange(3);
 		
-			car.gearChange( 3 );
+		// 車クラスのrunメソッドを実行する。
+		speed.run();
 		
-			car.run(  );
-		}
 	}
+
+}
--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -1,18 +1,43 @@
 package kadai_014;
 
-	public class Car_Chapter14
-{
-		//車クラスのフィールド
-		private int gear  = 1; // 1速から5速のギアを表す
-        private int speed = 10; //ギアチェンジ後の速度を表す
-		
-		 //車クラスのgearchangeメソッド
-		public void gearChange( int afterGear ) {
-			System.out.println( this.gear + "から" + afterGear  + "に切り替えました");
-		}
-		
-		//車クラスのrunメソッド
-		public void run() {
-			System.out.println("速度は時速" + this.speed + "kmです");
-	    }
+// 車クラス
+public class Car_Chapter14 {
+    // フィールド（内部データ）
+    private int gear =  1;    // 1速から5速のギアを表す
+    private int speed = 10;    // ギアチェンジ後の速度を表す    
+    private int previousGear = 1;    // 切り替え前のギアの値
+
+    // コンストラクタ（初期化処理）
+    public Car_Chapter14(int gear, int speed) {
+        this.gear = gear;
+        this.speed = speed;
+    }
+
+    public int getGear() {
+        return this.gear;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    // メソッド定義
+    public void gearChange(int afterGear) {
+        this.previousGear = this.gear;
+        this.gear = afterGear;
+        // ギアの値により速度を変える
+        this.speed = switch (this.gear) {
+            case 1 -> 10;
+            case 2 -> 20;
+            case 3 -> 30;
+            case 4 -> 40;
+            case 5 -> 50;
+            default -> 10;
+        };
+    }
+
+    public void run() {
+        System.out.println("ギア" + this.previousGear + "から" + this.getGear() + "に切り替えました");
+        System.out.println("速度は時速" + this.getSpeed() + "kmです");
+    }
 }
