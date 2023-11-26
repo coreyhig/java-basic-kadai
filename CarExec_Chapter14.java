e7b3fa6e1c10861446a268922b91d7fdac1f3b6e
coreyhig <143170162+coreyhig@users.noreply.github.com>
2023-11-26 17:04:27 +0900

    Add files via upload

--- CarExec_Chapter14.java
+++ CarExec_Chapter14.java
@@ -2,7 +2,7 @@ package kadai_014;
 	public class CarExec_Chapter14{
 		public static void main(String[] args) {
 			
-			Car_Chapter14 car = new Car_Chapter14();
+			Car_Chapter14 car = new Car_Chapter14(0,0);
 			
 			car.gearChange(3);
 			car.run();
--- Car_Chapter14.java
+++ Car_Chapter14.java
@@ -12,17 +12,16 @@ public class Car_Chapter14{
 	    }
 	    public void gearChange( int afterGear ) {
         System.out.println("ギア"+gear +"から" +afterGear + "に切り替わりました");
-        		{
-        		afterGear = speed;
-	    	switch(afterGear) {
-	    	case 1  -> System.out.println(10);
-	        case 2  -> System.out.println(20);
-	        case 3  -> System.out.println(30);
-	        case 4  -> System.out.println(40);
-	        case 5  -> System.out.println(50);
-	        case 6  -> System.out.println(60);
-	        default -> System.out.println(10);
-	    	}}}
+        		
+	    	speed = switch(afterGear) {
+	    	case 1  -> 10;
+	        case 2  -> 20;
+	        case 3  -> 30;
+	        case 4  -> 40;
+	        case 5  -> 50;
+	        case 6  -> 60;
+	        default -> 10;
+	    	};
 	    public void run() {
 	    	System.out.println("速度は時速" + speed + "kmです");
 	    }
