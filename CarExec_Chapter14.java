0779a004273c2e224e645b4ff7528f171498f7a4
coreyhig <143170162+coreyhig@users.noreply.github.com>
2023-11-26 16:37:29 +0900

    Add files via upload

--- CarExec_Chapter14.java
+++ CarExec_Chapter14.java
@@ -2,10 +2,10 @@ package kadai_014;
 	public class CarExec_Chapter14{
 		public static void main(String[] args) {
 			
-
-			Car_Chapter14 gear = new Car_Chapter14(1,10);
+			Car_Chapter14 car = new Car_Chapter14();
 			
-			gear.gearChange(3);
+			car.gearChange(3);
+			car.run();
 			
 			}
 			
--- Car_Chapter14.java
+++ Car_Chapter14.java
@@ -13,16 +13,17 @@ public class Car_Chapter14{
 	    public void gearChange( int afterGear ) {
         System.out.println("ギア"+gear +"から" +afterGear + "に切り替わりました");
         		{
+        		afterGear = speed;
 	    	switch(afterGear) {
-	    	case 1  -> System.out.println("速度は時速10kmです");
-	        case 2  -> System.out.println("速度は時速20kmです");
-	        case 3  -> System.out.println("速度は時速30kmです");
-	        case 4  -> System.out.println("速度は時速40kmです");
-	        case 5  -> System.out.println("速度は時速50kmです");
-	        case 6  -> System.out.println("速度は時速50kmです");
-	        default -> System.out.println("速度は時速100kmです");
-	    	}
-	    	}
-        		}
+	    	case 1  -> System.out.println(10);
+	        case 2  -> System.out.println(20);
+	        case 3  -> System.out.println(30);
+	        case 4  -> System.out.println(40);
+	        case 5  -> System.out.println(50);
+	        case 6  -> System.out.println(60);
+	        default -> System.out.println(1000);
+	    	}}}
+	    public void run() {
+	    	System.out.println("速度は時速" + speed + "kmです");
 	    }
-	      
+        		}
