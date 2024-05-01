37f0d5eb5965b87556244e2c892fb5d22d771ecc
coreyhig <143170162+coreyhig@users.noreply.github.com>
2024-05-01 14:25:23 +0900

    Add files via upload

---
+++ kadai026/JyankenExec_Chapter26.java
@@ -0,0 +1,10 @@
+package kadai026;
+
+public class JyankenExec_Chapter26 {
+	public static void main(String[] args) {
+		Jyanken_Chapter26 jyanken = new Jyanken_Chapter26();
+		String myChoice = jyanken.getMyChoice();
+		String random = jyanken.getRandom();
+		jyanken.playGame(myChoice, random);
+	}
+}
